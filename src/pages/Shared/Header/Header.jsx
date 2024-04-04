a59fc8e4c91a86ee3ca597b9e1dd7cfe153bd565
mahade <mahade.adib45@gmail.com>
2024-04-04 16:28:39 +0600

    breaking news and navbar section added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -3,10 +3,10 @@ import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='text-center my-3'>
+            <img className='mx-auto my-3' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
-            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
+            <p className='text-xl my-2'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
