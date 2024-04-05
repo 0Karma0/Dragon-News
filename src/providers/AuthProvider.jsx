74b3576fb07a3e6ec81e06d23713bfe1a9b4ad67
mahade <mahade.adib45@gmail.com>
2024-04-05 12:06:06 +0600

    register and login auth added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,12 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
+// eslint-disable-next-line react/prop-types
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
 
@@ -13,9 +14,32 @@ const AuthProvider = ({children}) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                setUser(result.user);
+            })
+    };
+
+    const logOut = () =>{
+        return signOut(auth);
+    }
+
+    useEffect(() =>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log('user in the auth state', currentUser);
+            setUser(currentUser);
+        });
+        return () =>{
+            unSubscribe();
+        }
+    }, [])
+
     const authInfo = {
         user,
-        createUser
+        createUser,
+        signIn,
+        logOut
     }
 
     return (
